8d74a6b4a81ce31374aa35d2b97a64efe62c3dc5
ejshafran <ejshafran@hilma.tech>
2025-08-02 11:46:10 +0300

    Read file

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Fprintln(os.Stderr, "Missing required argument: filename")
+		os.Exit(1)
+		return
+	}
+
+	filename := args[0]
+
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+		return
+	}
+
+	fmt.Print(string(file))
 }
